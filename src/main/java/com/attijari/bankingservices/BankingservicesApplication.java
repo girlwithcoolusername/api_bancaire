2233de03ce2708cdd7726a4ee2f71df4d6a2d68a
HP <manonkrk99@gmail.com>
2024-04-12 22:39:50 +0000

    V2.1 After HTTPs

--- src/main/java/com/attijari/bankingservices/BankingservicesApplication.java
+++ src/main/java/com/attijari/bankingservices/BankingservicesApplication.java
@@ -1,7 +1,11 @@
 package com.attijari.bankingservices;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankingservicesApplication {
@@ -9,5 +13,17 @@ public class BankingservicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankingservicesApplication.class, args);
 	}
-
+	private Connector connector(){
+		Connector connector = new Connector (TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setPort(8080);
+		connector.setSecure (false);
+		connector.setScheme("http");
+		return connector;
+	}
+	@Bean
+	public ServletWebServerFactory servletWebServletFactory(){
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addAdditionalTomcatConnectors (connector());
+		return factory;
+	}
 }
